605f7b33857b6f80e0a252adbe73ecc0b3ce89ba
laffingragons <akshaypatil,vit@gmail.com>
2018-07-02 10:56:58 +0530

    create and get all blogs ready

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,13 +1,107 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const shortid = require('short-id')
 
 // importing model here
 const BlogModel = mongoose.model('Blog')
 
 
+    let getAllBlog = (req, res) => {
+        
+        BlogModel.find()
+            .select('-__v -_id')
+            .lean()
+            .exec((err, result) =>{
+
+                if(err){
+
+                    console.log(err);
+                    res.send(err);
+                    
+                }else if(result == undefined || result == null || result == ''){
+
+                    console.log('Blog not Found!!!');
+                    res.send('No Blog found!');
+                    
+                }else{
+
+                    res.send(result);
+
+                }
+            })
+
+    }//get all blogs ends here
+
+
+    // Function to get a single blog by Id
+    let viewByBlogId = (req, res) => {
+
+        BlogModel.findOne({ 'blogId': req.params.blogId }, (req, res) => {
+
+            if(err){
+
+                console.log(err);
+                res.send(err);
+
+            }else if(result == undefined || result == null || result == ''){
+
+                console.log('Blog not Found!!!');
+                res.send('No Blog found!');
+                
+            }else{
+
+                res.send(result);
+
+            }
+
+        })
+
+    }//end of get single blog
+
+    let createBlog = (req, res) => {
+
+        var today = Date.now();
+        let blogId = shortid.generate();
+       
+        let newBlog = new BlogModel({
+
+            blogId : blogId,
+            title : req.body.title,
+            description : req.body.description,
+            bodyHtml : req.body.blogBody,
+            isPublished : true,
+            category : req.body.category,
+            author : req.body.fullName,
+            created : today,
+            lastModifies : today
+
+        })// end of new blog
+
+        let tags = (req.body.tags != undefined && req.body.tags != null && req.body.tags != '') ? req.body.tags.split(',') : []
+        newBlog.tags = tags;
+
+        newBlog.save((err, result) => {
+
+            if(err){
+
+                console.log(err);
+                res.send(err);
+
+            }else{
+
+                res.send(result);
+
+            }
+
+        }) // end of save blog function
+
+    }//end of blog create function
 
 module.exports = {
 
-  
-    
+  getAllBlog: getAllBlog,
+  viewByBlogId: viewByBlogId,
+  createBlog: createBlog,
+
+
 }
--- routes/bolgs.js
+++ routes/bolgs.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const blogController = require('./../controllers/blogController')
+const apiConfig = require('./../config/appConfig')
 
 let setRouter = (app) => {
 
-       
+    let baseUrl = apiConfig.apiVersion+'/blogs';
+
+       app.get(baseUrl+'/all', blogController.getAllBlog);
+
+       app.get(baseUrl+'/view/:blogId', blogController.viewByBlogId);
+
+       app.post(baseUrl+'/create', blogController.createBlog);
 
     } // end of setROuter function
 
